8b15f2fecaf5a3700b93ccbc0edf664728dafdc1
Kenia <keniayhdiaz@gmail.com>
2024-10-11 11:43:25 -0500

    feat: :zap: Creación de una nueva entidad Direccion

---
+++ src/main/java/com/example/proyect/demo/domain/entities/Direccion.java
@@ -0,0 +1,45 @@
+package com.example.proyect.demo.domain.entities;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "direccion")
+public class Direccion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 100)
+    @NotNull
+    private String calle;
+
+    @Column(length = 100)
+    @NotNull
+    private String carrera;
+
+    @Column(length = 200)
+    private String descripcion;
+
+    @ManyToOne
+    @JoinColumn(name = "id_ciudad")
+    private Ciudad ciudad;
+
+    @OneToMany(mappedBy = "direccion")
+    private List<Sucursal> sucursal;
+    
+
+
+
+}
