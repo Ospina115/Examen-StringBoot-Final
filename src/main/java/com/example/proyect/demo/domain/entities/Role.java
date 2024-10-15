bc46fcd5ae8e97685b76da770f81197d0681af05
Kenia <keniayhdiaz@gmail.com>
2024-10-15 12:27:11 -0500

    feat: :sparkles: Comienzo de implemetanción JWT

---
+++ src/main/java/com/example/proyect/demo/domain/entities/Role.java
@@ -0,0 +1,32 @@
+package com.example.proyect.demo.domain.entities;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @JsonIgnoreProperties({"roles", "handler", "hibernateLazyInitializer"}) //previene el error de serialización y la información ciclica.
+    @ManyToMany(mappedBy = "roels")
+    private List<User> users;
+
+
+}
---
+++ src/main/java/com/example/proyect/demo/infrastructure/repositories/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.proyect.demo.infrastructure.repositories.user;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.proyect.demo.domain.entities.User;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUsername(String username); 
+}
