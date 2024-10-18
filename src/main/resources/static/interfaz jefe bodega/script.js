2f173fb0a8b99e4772542eeb5eef0fbde0c76619
Samuel <samueljov31@gmail.com>
2024-10-18 12:14:49 -0500

    style: :art:
    Finalizado el modulo Jefe de Bódega

--- src/main/resources/static/interfaz jefe bodega/script.js
+++ src/main/resources/static/interfaz jefe bodega/script.js
@@ -1,26 +1,59 @@
 class RegisterForm extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <form>
-                <h2>Registrar Insumos</h2>
-                <label for="insumo">Insumo:</label>
-                <input type="text" id="insumo" name="insumo" required>
+        <form id="register-form">
+            <h2>Registrar Insumo</h2>
+            <input placeholder="Codigo Interno" type="text" id="insumo" name="insumo" required>
+            <input placeholder="Nombre del Insumo" type="text" id="nombre" name="nombre">
+            <input placeholder="Valor Unidad" type="number" id="precio" name>
+            <input placeholder="Cantidad" type="number" id="cantidad" name>
+            <input placeholder="Stock Minimo" type="number" id="stock_min" name>
+            <input placeholder="Stock Maximo" type="number" id="stock_max" name>
+            <div class="buttons">
                 <button type="submit">Registrar</button>
-            </form>
+                <button type="button" id="clear-button">Limpiar</button>        
+            </div>
+
+        </form>
         `;
+
+        this.querySelector('#clear-button').addEventListener('click', () => {
+            this.resetForm();
+        });
+    }
+
+    resetForm() {
+        const form = this.querySelector('#register-form');
+        form.reset(); // Resetea todos los campos del formulario
     }
 }
 
 class EditForm extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <form>
+            <form id="edit-form">
                 <h2>Editar Insumos</h2>
-                <label for="insumo">Insumo:</label>
-                <input type="text" id="insumo" name="insumo" required>
-                <button type="submit">Editar</button>
+                <input placeholder="Codigo Interno" type="text" id="insumo" name="insumo" required>
+                <input placeholder="Nombre del Insumo" type="text" id="nombre" name="nombre">
+                <input placeholder="Valor Unidad" type="number" id="precio" name>
+                <input placeholder="Cantidad" type="number" id="cantidad" name>
+                <input placeholder="Stock Minimo" type="number" id="stock_min" name>
+                <input placeholder="Stock Maximo" type="number" id="stock_max" name>    
+                <div class="buttons">
+                    <button type="submit">Editar</button>
+                    <button type="button" id="clear-button">Limpiar</button>        
+                </div>
             </form>
         `;
+
+        this.querySelector('#clear-button').addEventListener('click', () => {
+            this.resetForm();
+        });
+    }
+
+    resetForm() {
+        const form = this.querySelector('#edit-form');
+        form.reset(); // Resetea todos los campos del formulario
     }
 }
 
